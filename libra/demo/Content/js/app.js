0c33336ad66bbd9d2c3264e0addea26de394e1de
chclock <chwhu009@gmail.com>
2018-03-05 16:43:31 +0800

    m

--- libra/demo/Content/js/app.js
+++
@@ -1,9 +0,0 @@
-$(".btn").click(function(){
-    $.ajax({
-        url: "/blog/libra/17",
-        success: function(data){
-            var str = JSON.stringify(data);
-            alert(str);
-        }
-    });
-});
